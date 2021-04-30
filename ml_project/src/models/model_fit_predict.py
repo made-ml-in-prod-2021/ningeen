f6f67743c7acb7fdb304b15d442a2504e2a88038
ningeen <ningenkun@gmail.com>
2021-04-30 17:10:20 +0300

    make models work

--- ml_project/src/models/model_fit_predict.py
+++ ml_project/src/models/model_fit_predict.py
@@ -7,16 +7,16 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score, accuracy_score
 
-from src.entities import TrainingParams
+from entities import TrainingParams
 
 SklearnClassifierModel = Union[RandomForestClassifier, LogisticRegression]
 
 
 def get_model(params: TrainingParams) -> SklearnClassifierModel:
     if params.model_type == "RandomForestClassifier":
-        model = RandomForestClassifier(**params.rf_params)
+        model = RandomForestClassifier(**params.clf_params)
     elif params.model_type == "LogisticRegression":
-        model = LogisticRegression(**params.lr_params)
+        model = LogisticRegression(**params.clf_params)
     else:
         raise NotImplementedError(f"Model type '{params.model_type}' not implemented")
     return model
