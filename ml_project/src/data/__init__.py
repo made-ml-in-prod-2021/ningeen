ce92e9950a3a36e96e4bd6394c46307a830f7bdf
ningeen <ningenkun@gmail.com>
2021-05-11 20:41:34 +0300

    remove hw1

--- ml_project/src/data/__init__.py
+++
@@ -1,3 +0,0 @@
-from .make_dataset import split_train_val_data, read_data
-
-__all__ = ["split_train_val_data", "read_data"]
--- ml_project/src/entities/train_params.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass, field
-
-
-@dataclass()
-class TrainingParams:
-    model_type: str = field(default="RandomForestClassifier")
-    clf_params: dict = field(default_factory=dict)
