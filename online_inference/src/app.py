3f718661531188027b9ebd014eab9464a00dc439
ningeen <ningenkun@gmail.com>
2021-06-21 22:25:59 +0300

    probes pod

--- online_inference/src/app.py
+++ online_inference/src/app.py
@@ -1,10 +1,12 @@
 import logging
 import os
 import sys
+import time
 from typing import List
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
 
 from src.api import Predictor, InputStruct, OutputStruct, make_prediction, Validator
 
@@ -17,6 +19,9 @@ predictor: Predictor = None
 DEFAULT_PATH = "configs/config_pred.yaml"
 
 app = FastAPI()
+START_DELAY_SECONDS = 30
+DEATH_DELAY_TIME = 60
+START_TIME = time.time()
 
 
 @app.get("/")
@@ -27,6 +32,7 @@ def main():
 @app.on_event("startup")
 def load_predictor():
     global predictor
+    time.sleep(START_DELAY_SECONDS)
     params_path = os.getenv("PATH_TO_PARAMS")
     if params_path is None:
         logger.warning("No PATH_TO_PARAMS found, using default path.")
@@ -39,6 +45,21 @@ def load_predictor():
 def predict(request: InputStruct):
     Validator(request).validate_input()
     response = make_prediction(request.data, request.features, predictor)
+    return response
+
+
+@app.get("/readiness")
+def readiness() -> bool:
+    return predictor is not None
+
+
+@app.get("/liveness")
+def liveness() -> PlainTextResponse:
+    work_time = time.time() - START_TIME
+    if work_time < DEATH_DELAY_TIME:
+        response = PlainTextResponse("Live", status_code=200)
+    else:
+        response = PlainTextResponse("Dead", status_code=400)
     return response
 
 
